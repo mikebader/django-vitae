a9882e12d78a9e610321292198fd3e954d868d56
Mike Bader <michaeldmbader@gmail.com>
2018-11-30 18:00:40 -0500

    tested accomplishment models

--- cv/models/accomplishments.py
+++ cv/models/accomplishments.py
@@ -24,7 +24,7 @@ class Award(DisplayableModel):
         return self.name
 
     def get_absolute_url(self):
-        return '{}#award-{}}'.format(reverse('cv:cv_list'), self.pk)
+        return '{}#award-{}'.format(reverse('cv:cv_list'), self.pk)
 
     objects = models.Manager()
 
@@ -32,16 +32,17 @@ class Award(DisplayableModel):
 class Degree(DisplayableModel):
     """Degree earned.
 
-    Degrees are sorted by ``end_date``.
+    Degrees are sorted in reverse order by ``end_date``.
     """
 
     degree = models.CharField(max_length=10)
     major = models.CharField(max_length=100, null=True, blank=True)
-    date_earned = models.DateField('Date Earned')
+    date_earned = models.DateField(_('Date Earned'))
     institution = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-    state = models.CharField('State or Province', max_length=100)
-    country = models.CharField(max_length=100)
+    state = models.CharField(
+        _('State or Province'), max_length=100, blank=True)
+    country = models.CharField(max_length=100, blank=True)
     honors = models.CharField(max_length=100, null=True, blank=True)
 
     class Meta:
@@ -58,7 +59,7 @@ class Degree(DisplayableModel):
 
 
 class Position(DisplayableModel):
-    """osition of employment or research experience.
+    """Position of employment or research experience.
 
     Positions are sorted by ``end_date``.
 
@@ -69,8 +70,8 @@ class Position(DisplayableModel):
     """
 
     title = models.CharField(max_length=100)
-    start_date = models.DateField()
-    end_date = models.DateField(
+    start_date = models.DateField(null=False)
+    end_date = models.DateField(null=True, blank=True,
         help_text=_('If current, set date to future (by default positions '
                     'will be ordered by end date')
     )
@@ -78,33 +79,34 @@ class Position(DisplayableModel):
     department = models.CharField(max_length=100, blank=True)
     institution = models.CharField(max_length=100)
     current_position = models.BooleanField(
-        _('Current position?'),
+        _('Current position?'), default=False,
         help_text=_('Are you currently in this position?'))
     primary_position = models.BooleanField(
-        _('Primary position?'),
+        _('Primary position?'), default=False,
         help_text=_('Should this position be displayed as the main position '
                     '(e.g., on heading of CV)?')
     )
 
     def clean(self):
         """Ensure start date is before end date."""
-        if self.start_date > self.end_date:
-            raise ValidationError(
-                {'end_date': _('End date cannot be before start date')}
-            )
+        if self.start_date and self.end_date:
+            if self.start_date > self.end_date:
+                raise ValidationError(
+                    {'end_date': _('End date cannot be before start date')}
+                )
 
     def get_absolute_url(self):
         return '{}#position-{}'.format(reverse('cv:cv_list'), self.pk)
 
     class Meta:
-        ordering = ('-end_date',)
+        ordering = ['-end_date', '-start_date']
         get_latest_by = 'end_date'
 
     def __str__(self):
         return '%s' % (self.title)
 
     objects = models.Manager()
-    primarypositions = PrimaryPositionManager()
+    primary_positions = PrimaryPositionManager()
 
 
 class MediaMention(DisplayableModel):
---
+++ tests/test_accomplishments.py
@@ -0,0 +1,114 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from django.utils.translation import ugettext_lazy as _
+
+from nose.plugins.attrib import attr
+# from unittest import TestCase
+
+from cv.models import Award, Degree, Position
+
+
+@attr('accomplishments')
+class AccomplishmentsTestCase(TestCase):
+
+    # Awards
+    def test_award(self):
+        """Test creation, string representation, and url of award."""
+        award = Award.objects.create(
+            name='award', organization='important place', date='2000-01-01')
+        self.assertEqual(str(award), 'award')
+        self.assertEqual(award.get_absolute_url(), '/#award-1')
+
+    def test_award_displayable(self):
+        """Test that only displayable awards are in displayable manager."""
+        Award.objects.create(
+            name='award', organization='important place', date='2000-01-01',
+            display=False)
+        self.assertEqual(len(Award.displayable.all()), 0)
+
+    # Degrees
+    def test_degree(self):
+        """Test creation, string representation, and url of degrees."""
+        degree = Degree.objects.create(
+            degree='B.A.', date_earned='2000-01-01',
+            institution='college', city='anywhere', state='state')
+        self.assertEqual(str(degree), 'B.A.')
+        self.assertEqual(degree.get_absolute_url(), "/#degree-1")
+
+    def test_degree_displayable(self):
+        """Test that only dispayable degrees are in displayable manager."""
+        Degree.objects.create(
+            degree='B.A.', date_earned='2000-01-01',
+            institution='college', city='anywhere', state='state',
+            display=False)
+        self.assertEqual(len(Degree.displayable.all()), 0)
+
+    def test_degree_ordering(self):
+        """Test reverse chronological ordering of degrees."""
+        Degree.objects.create(
+            degree='B.A.', date_earned='2000-01-01',
+            institution='college', city='anywhere', state='state')
+        Degree.objects.create(
+            degree='M.A.', date_earned='2001-01-01',
+            institution='college', city='anywhere', state='state')
+        degrees = [str(degree) for degree in Degree.displayable.all()]
+        self.assertEqual(degrees, ['M.A.', 'B.A.'])
+        self.assertEqual(str(Degree.displayable.all().latest()), 'M.A.')
+
+    # Positions
+    def test_position(self):
+        """Test creation, string representation, and url of positions."""
+        position = Position.objects.create(
+            title='Fancy title', institution='Fancy Institution',
+            start_date='2002-01-01')
+        self.assertEqual(str(position), 'Fancy title')
+        self.assertEqual(position.get_absolute_url(), "/#position-1")
+
+    def test_position_dates(self):
+        """Positions require start date & end date must be after start date."""
+        position_fields = {'title': 'Fancy Title', 'institution': 'Institute'}
+        try:
+            Position.objects.create(**position_fields)
+        except ValidationError as e:
+            self.assertTrue('start_date' in e.error_dict.keys())
+            self.assertIn('This field cannot be null',
+                          str(e.error_dict['start_date'][0]))
+        position_fields.update({'end_date': '2000-01-01',
+                                'start_date': '2010-01-01'})
+        with self.assertRaises(ValidationError) as err:
+            Position.objects.create(**position_fields)
+        e = err.exception
+        self.assertTrue('end_date' in e.error_dict.keys())
+        self.assertIn('End date cannot be before start date',
+                      str(e.error_dict['end_date']))
+
+    def test_primary_position(self):
+        """Primary position manager should only return primary positions."""
+        Position.objects.create(
+            title='Fancy title', institution='Fancy Institution',
+            start_date='2002-01-01', primary_position=True)
+        primary_positions = Position.primary_positions.all()
+        self.assertEqual(len(primary_positions), 1)
+        Position.objects.create(
+            title='Fancier title', institution='Fancier Institution',
+            start_date='2003-01-01')
+        primary_positions = Position.primary_positions.all()
+        self.assertEqual(len(primary_positions), 1)
+
+
+    def test_position_ordering(self):
+        """Positions should be reverse chronologically ordered."""
+        Position.objects.create(
+            title='Fancy title', institution='Fancy Institution',
+            start_date='2002-01-01', end_date='2002-12-31')
+        Position.objects.create(
+            title='Fancier title', institution='Fancier Institution',
+            start_date='2003-01-01', end_date='2004-01-01')
+        positions = [str(position) for position in Position.objects.all()]
+        self.assertEqual(['Fancier title', 'Fancy title'], positions)
+        self.assertEqual('Fancier title', str(Position.objects.all().latest()))
+
+
+
+
+
